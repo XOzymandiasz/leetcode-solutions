3fdbf48c74740a1ceb1470d8bae2efcb49378703
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-03-20 21:01:23 +0100

    feat(go): add solution for minimumChangesToMakeAlternatingBinaryString (#1758)

---
+++ 0001758_minimumChangesToMakeAlternatingBinaryString/minOperations.go
@@ -0,0 +1,23 @@
+package _001758_minimumChangesToMakeAlternatingBinaryString
+
+const ZERO = '0'
+const ONE = '1'
+
+func MinOperations(s string) int {
+	counter := 0
+	var last rune
+	for _, r := range s {
+		if r == last {
+			counter++
+			if r == ZERO {
+				last = ONE
+			} else {
+				last = ZERO
+			}
+		} else {
+			last = r
+		}
+	}
+
+	return min(counter, len(s)-counter)
+}
