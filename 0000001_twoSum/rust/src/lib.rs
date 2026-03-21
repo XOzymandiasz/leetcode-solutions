c18a009a5fe48a7931bb9991e22fc1e0c37c8524
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-03-21 01:31:55 +0100

    docs(rust): add function description

--- 0000001_twoSum/rust/src/lib.rs
+++ 0000001_twoSum/rust/src/lib.rs
@@ -1,3 +1,6 @@
+// two_sum returns indices of two numbers that they add up to target.
+// Uses a map to store visited numbers and find complements in O(1),
+// resulting in an overall O(n) time complexity.
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     use std::collections::HashMap;
     let mut visited: HashMap<i32, i32> = HashMap::with_capacity(nums.len());
