52f253c5f6264635429d4a7ceed3354f3372aae9
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-03-21 19:44:40 +0100

    docs(go): modify method description

--- 0000001_twoSum/go/solution.go
+++ 0000001_twoSum/go/solution.go
@@ -1,8 +1,8 @@
 package twosum
 
-// TwoSum returns indices of two numbers that they add up to target.
+// TwoSum returns indices of two numbers whose sum equals the target.
 // Uses a map to store visited numbers and find complements in O(1),
-// resulting in an overall O(n) time complexity.
+// resulting in an overall O(n) time complexity and O(n) space complexity.
 func TwoSum(nums []int, target int) []int {
 	visited := make(map[int]int, len(nums))
 
