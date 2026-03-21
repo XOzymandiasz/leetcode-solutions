985f7d141ebfe5c71a7960e24e62fd4f7f7ca1de
adamk <adam.klamrowski.ak@gmail.com>
2026-03-21 23:44:25 +0100

    refactor(cpp): instead od static cast nums size, static_cast final index

--- 0000001_twoSum/cpp/solution.cpp
+++ 0000001_twoSum/cpp/solution.cpp
@@ -4,19 +4,17 @@
 
 #include "solution.hpp"
 
-using namespace std;
-
 // twoSum returns indices of two numbers whose sum equals the target.
 // Uses a hash map to store visited numbers and find complements in O(1),
 // resulting in an overall O(n) time complexity and O(n) space complexity.
 std::vector<int> Solution::twoSum(std::vector<int>& nums, int target) {
     std::unordered_map<int, int> m;
 
-    for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
+    for (size_t i = 0; i < nums.size(); ++i) {
         int complement = target - nums[i];
 
         if (m.find(complement) != m.end()) {
-            return {m[complement], i};
+            return {m[complement], static_cast<int>(i)};
         }
 
         m[nums[i]] = i;
