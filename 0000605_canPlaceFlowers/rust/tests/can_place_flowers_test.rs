c060cc43814091701194ad654403c64cb81aae86
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-03-23 00:46:16 +0100

    test(rust): test solution for canPlaceFlowers (#605)

--- 0000605_canPlaceFlowers/rust/tests/can_place_flowers_test.rs
+++ 0000605_canPlaceFlowers/rust/tests/can_place_flowers_test.rs
@@ -2,4 +2,25 @@ use rust::can_place_flowers;
 
 #[test]
 fn base() {
+    assert_eq!(true, can_place_flowers(vec![1,0,0,0,1], 1));
+}
+
+#[test]
+fn to_many_flowers() {
+    assert_eq!(false, can_place_flowers(vec![1,0,0,0,1], 2));
+}
+
+#[test]
+fn one_element() {
+    assert_eq!(false, can_place_flowers(vec![1], 1));
+}
+
+#[test]
+fn one_element_with_no_one_to_set() {
+    assert_eq!(true, can_place_flowers(vec![1], 0));
+}
+
+#[test]
+fn one_element_without_flowers() {
+    assert_eq!(true, can_place_flowers(vec![0], 1));
 }
