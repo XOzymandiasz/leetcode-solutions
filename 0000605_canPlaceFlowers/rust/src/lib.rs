a84b844c03e7b60cbe20f0f9b014d03920868104
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-03-23 00:45:42 +0100

    feat(rust): add solution for canPlaceFlowers (#605)

--- 0000605_canPlaceFlowers/rust/src/lib.rs
+++ 0000605_canPlaceFlowers/rust/src/lib.rs
@@ -1,18 +1,27 @@
+const PLANTED: i32 = 1;
+
 pub fn can_place_flowers(mut flowerbed: Vec<i32>,mut n: i32) -> bool {
-    for mut i in  0..=flowerbed.len()-1 {
-        if flowerbed[i+1] == 1 {
+    let len = flowerbed.len();
+    let mut index = 0;
+    if n == 0 { return true; }
+    while index < len {
+        let has_prev_flower = index > 0 && flowerbed[index - 1] == PLANTED;
+        let has_next_flower = index < len -1 && flowerbed[index + 1] == PLANTED;
+
+        if flowerbed[index] == PLANTED {
+            index += 2;
             continue;
         }
-        if flowerbed[i] == 1 {
-            i+=1;
+        if has_prev_flower || has_next_flower {
+            index += 1;
+            continue;
         }
+        flowerbed[index] = PLANTED;
+        index += 2;
+        n-=1;
         if n <= 0 {
             return true;
         }
-        n-=1;
-        flowerbed[i] = 1;
-        i+=1;
     }
-
     false
 }
