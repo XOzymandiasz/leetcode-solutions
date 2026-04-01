f288e91ec4f14a60638fb5a7d4668cffc83539bc
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-04-01 02:21:27 +0200

    test(go): test solution for greatestCommonDivisorOfStrings (#1071)

---
+++ 0001071_greatestCommonDivisorOfStrings/go/solution_test.go
@@ -0,0 +1,30 @@
+package greatestCommonDivisors_test
+
+import (
+	"testing"
+
+	solution "github.com/XOzymandiasz/leetcode-solutions/0001071_greatestCommonDivisorOfStrings/go"
+)
+
+func TestGcdOfStrings(t *testing.T) {
+	tests := []struct {
+		name string
+		str1 string
+		str2 string
+		want string
+	}{
+		{name: "basic example", str1: "ABCABC", str2: "ABC", want: "ABC"},
+		{name: "odd number of letters", str1: "ABABAB", str2: "ABAB", want: "AB"},
+		{name: "same strings", str1: "ZUBR", str2: "ZUBR", want: "ZUBR"},
+		{name: "one letter not odd", str1: "AAAAA", str2: "AA", want: "A"},
+		{name: "no divisor", str1: "LEET", str2: "CODE", want: ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solution.GcdOfStrings(tt.str1, tt.str2); got != tt.want {
+				t.Fatalf("gcdOfStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
