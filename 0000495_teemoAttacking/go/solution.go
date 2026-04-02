d0bf468319193bc2456531b930ca9d58285f47c0
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-04-02 17:10:30 +0200

    feat(go): add solution teemoAttacking (#495)

---
+++ 0000495_teemoAttacking/go/solution.go
@@ -0,0 +1,17 @@
+package teemoAttacking
+
+func findPoisonedDuration(timeSeries []int, duration int) int {
+	totalDuration := 0
+	n := len(timeSeries)
+
+	for i := 0; i < n-1; i++ {
+		interval := timeSeries[i+1] - timeSeries[i]
+		if (interval) > duration {
+			totalDuration += duration
+		} else {
+			totalDuration += interval
+		}
+	}
+
+	return totalDuration + duration
+}
