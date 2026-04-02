5f450aa8ed52db3e07c8a00b1540e3872d961b8c
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-04-02 17:11:08 +0200

    feat(go): add solution maximumAverageSubarray (#643)

---
+++ 0000643_maximumAverageSubarray/go/solution.go
@@ -0,0 +1,17 @@
+package maxAverageSubArray
+
+func FindMaxAverage(nums []int, k int) float64 {
+	maxSum := 0
+	actualSum := 0
+
+	for i := 0; i < k; i++ {
+		actualSum += nums[i]
+	}
+	maxSum = actualSum
+	for j := 1; j <= len(nums)-k; j++ {
+		actualSum = actualSum - nums[j-1] + nums[j+k-1]
+		maxSum = max(maxSum, actualSum)
+	}
+
+	return float64(maxSum) / float64(k)
+}
