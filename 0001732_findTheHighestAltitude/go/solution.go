5b7242d38c5453915585b2ec4f44ab374b565b2b
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-04-11 01:39:20 +0200

    fix(go): use value instead of index in range loop

--- 0001732_findTheHighestAltitude/go/solution.go
+++ 0001732_findTheHighestAltitude/go/solution.go
@@ -4,7 +4,7 @@ func LargestAltitude(gain []int) int {
 	altitude := 0
 	highest := 0
 
-	for point := range gain {
+	for _, point := range gain {
 		altitude = altitude + point
 		highest = max(highest, altitude)
 	}
