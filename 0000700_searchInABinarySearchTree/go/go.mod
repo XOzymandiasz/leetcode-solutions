57bf414293abae4dd7178de1dacc9355996daf75
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-04-12 16:44:40 +0200

    feat(go): add solution for searchInABinarySearch (#700)

---
+++ 0000700_searchInABinarySearchTree/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/XOzymandiasz/leetcode-solutions/0000700_searchInABinarySearchTree/go
+
+go 1.26
---
+++ 0000700_searchInABinarySearchTree/go/solution.go
@@ -0,0 +1,21 @@
+package solution
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func SearchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == val {
+		return root
+	}
+	if root.Val < val {
+		return SearchBST(root.Right, val)
+	}
+
+	return SearchBST(root.Left, val)
+}
