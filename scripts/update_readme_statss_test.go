7566923da507f15f56d5410176912da702062886
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-04-17 01:38:38 +0200

    test: add simple test for update README

---
+++ scripts/update_readme_statss_test.go
@@ -0,0 +1,40 @@
+package main_test
+
+import (
+	"strings"
+	"testing"
+
+	updateReadme "github/leetcode-solutions/scripts"
+)
+
+func TestUpdateReadmeContent(t *testing.T) {
+	input := `
+		# Repo
+		
+		| Difficulty |
+		|------------|
+		| 🟢 |
+		| 🟢 |
+		| 🟡 |
+		| 🔴 |
+		
+		<!-- STATS_START -->
+		old stats
+		<!-- STATS_END -->
+	`
+
+	output := updateReadme.UpdateReadmeContent(input)
+
+	if !strings.Contains(output, "- Total solved: 4") {
+		t.Fatalf("expected total solved = 4, got:\n%s", output)
+	}
+	if !strings.Contains(output, "- Easy: 2 🟢") {
+		t.Fatalf("expected easy = 2, got:\n%s", output)
+	}
+	if !strings.Contains(output, "- Medium: 1 🟡") {
+		t.Fatalf("expected medium = 1, got:\n%s", output)
+	}
+	if !strings.Contains(output, "- Hard: 1 🔴") {
+		t.Fatalf("expected hard = 1, got:\n%s", output)
+	}
+}
