fa6f2e2cd119c8ce0463377716503474d6abecf0
XOzymandiasz <101061693+XOzymandiasz@users.noreply.github.com>
2026-04-16 23:35:03 +0200

    test(go): test solution for guessNumberHigherOrLower (#374)

---
+++ 0000374_guessNumberHigherOrLower/go/solution_test.go
@@ -0,0 +1,12 @@
+package solution_test
+
+import (
+	"testing"
+
+	solution "github.com/XOzymandiasz/leetcode-solutions/0000374_guessNumberHigherOrLower/go"
+)
+
+func TestGuessNumber(t *testing.T) {
+	got := solution.GuessNumber(2)
+	println(got)
+}
